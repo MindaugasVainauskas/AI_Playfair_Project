b80eb7b456299032bb49bacfc830f4fc5ba56d06
MindaugasVainauskas <g00327967@gmit.ie>
2018-03-08 16:07:59 +0000

    Added runner file to the project. deciphering when key is known now works

--- src/ie/gmit/sw/ai/PlayFairCipher.java
+++ src/ie/gmit/sw/ai/PlayFairCipher.java
@@ -5,10 +5,10 @@ public class PlayFairCipher {
 	private String cipherKey;	
 	
 	//hardcoded for now. will pull from file later 
-	private String encryptedMessage = "HFZQLYVEDWNITIQPQDUVHYLGXZHFNYBKPACAZQHFVQIQCUUVYCBXABQZQZURHQDZHBKDMVZQHXRGURLQHTXZQVDFYXZHRGGWHBYEGXNYYEGKYVHFLQDBWDVQIZEAUCAHHPQIBRRVBREZNYYQAHPUQDUVHYZXGNRDEOZWQFKCLZZHXVRDEOFEINQZZKZPKDYDCAMEEQUDBCLDBKPAEDUVYCHFZQQEUMSVPBUMURLQHTXZXZCUHTVTPHMDLDRGMDLDVBHCMGUVYCQVPVDMSZXQCPDIQZLQKDUBEMTCYDDBCQGDFEUKQZVPCYUHKDIABDFVFEETGKIDOZEFURLQUVYCKDPTACYQUCFUPVVBBREZZXDTZPWCMEDILYTHZHADMUDBGQHBKIFEMDEWIZRGVQHTKCNWIEGNHCPLLUDPCOFTQGDPNWBYHCHFQZITQVGKUVYCHFBDQVHVHCHFDIYXHFBRUMLZKDZDFQFHNYLGSAPLQCCAZQHCPCBODITCVBMUHFDIYXHFBRUMLZKDLULIDLIDDLQRKWZQACYQUZBHZBDUBHQZUKUZEDGWTVBXABQZQZBUFEUFFTQVEKZQINAHMEPTDFNYFBIZEXBRRVBREZTCILEVFBEDHUBRWDLYTHFHIZNYCPOVBDLIZQHFQPQDUVHYLGCUNYOKDMPCHTXZPCGCHFDYLQDBLTHPQEKCGKTIQIBRVQHBQNDBRXBZEFRFVUEDQYNYMZCPBDHYLKCUXF";
+	private String encryptedMessage;
 	
-	public PlayFairCipher() {
-		setCipherKey("THEQUICKBROWNFXMPDVLAZYGS");
+	public PlayFairCipher(String cKeyMatrix) {
+		setCipherKey(cKeyMatrix);
 	}	
 
 	//set cipher key to be the selected formatted keystring
@@ -16,55 +16,57 @@ public class PlayFairCipher {
 		this.cipherKey = keyString;
 	}
 	
+	public String getCipherKey() {
+		return this.cipherKey;
+	}
+
+	public String getEncryptedMessage() {
+		return encryptedMessage;
+	}
+
+	public void setEncryptedMessage(String encryptedMessage) {
+		this.encryptedMessage = encryptedMessage;
+	}
+
 	//create character pairs from ciphertext.
-	public String[] createDigrams() {
-		String digrams[] = new String[encryptedMessage.length() /2];
+	public String[] createDigrams(String encMessage) {
+		String digrams[] = new String[encMessage.length() /2];
 		int i = 0;
 		
-		for (int j = 0; j < encryptedMessage.length(); j = j + 2) {
-			 digrams[j] = encryptedMessage.substring(j, j + 2);
+		for (int j = 0; j < encMessage.length()-1; j = j + 2) {
+			 digrams[i] = encMessage.substring(j, j + 2);
 	         i++;
 		}
 		
 		return digrams;
 	}
-	
-	
 	//Decryption method to return plain text
 	public String DecryptCipherText(String[] digrams, String cKey) {
 		StringBuilder plainText = new StringBuilder();
+		System.out.println(cKey);
+		for (String pair : digrams) {
+			int row1 = (cKey.indexOf(pair.charAt(0)) / 5);
+            int col1 = (cKey.indexOf(pair.charAt(0)) % 5);
+            int row2 = (cKey.indexOf(pair.charAt(1)) / 5);
+            int col2 = (cKey.indexOf(pair.charAt(1)) % 5);
+            
+            char chr1;
+            char chr2;
+            
+            if (col1 == col2) {
+                chr1 = cKey.charAt((row1 + 4) % 5 * 5 + col1);
+                chr2 = cKey.charAt((row2 + 4) % 5 * 5 + col2);
+            } else if (row1 == row2) {
+                chr1 = cKey.charAt(row1 * 5 +(col1 +4) % 5);
+                chr2 = cKey.charAt(row2 * 5 +(col2 +4) % 5);
+            } else {
+                chr1 = cKey.charAt(row1 * 5 + col2);
+                chr2 = cKey.charAt(row2 * 5 + col1);
+            }
+            
+            plainText.append(Character.toString(chr1) + Character.toString(chr2));
+		}
 		
-		for (String digram : digrams) {
-			 byte row1 = (byte) (cipherKey.indexOf(digram.charAt(0)) / 5);
-	            byte col1 = (byte) (cipherKey.indexOf(digram.charAt(0)) % 5);
-	            byte row2 = (byte) (cipherKey.indexOf(digram.charAt(1)) / 5);
-	            byte col2 = (byte) (cipherKey.indexOf(digram.charAt(1)) % 5);
-	            //System.out.println("char 1 " + pair.charAt(0) + "  at " + row1 + "   " + col1);
-	            //System.out.println("char 2 " + pair.charAt(1) + "  at " + row2 + "   " + col2);
-
-	            char chr1;
-	            char chr2;
-	            if (col1 == col2) {
-	                chr2 = cipherKey.charAt(((row2 - 1) % 5 * 5 + col2));
-	                chr1 = cipherKey.charAt(((row1 - 1) % 5 * 5 + col1));
-	            } else if (row1 == row2) {
-	                chr1 = cipherKey.charAt(row1 * 5 + ((col1 - 1) % 5));
-	                chr2 = cipherKey.charAt(row2 * 5 + ((col2 - 1) % 5));
-	            } else {
-	                chr1 = cipherKey.charAt(row1 * 5 + col2);
-	                chr2 = cipherKey.charAt(row2 * 5 + col1);
-	            }
-	            plainText.append(Character.toString(chr1) + Character.toString(chr2));
-	        }
 		return plainText.toString();
 	}
-	
-	public static void main(String[] args) {
-		PlayFairCipher pfc = new PlayFairCipher();
-		
-		
-		//should return deciphered text
-		//pfc.DecryptCipherText(digrams, cKey)
-	}
-
 }
